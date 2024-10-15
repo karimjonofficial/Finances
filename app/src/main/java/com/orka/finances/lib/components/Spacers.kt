3e7bd946b918015a07398aed976bf92291d3e340
Karimjon <okracer3350@gmail.com>
2024-10-15 10:43:27 +0500

    Optimize imports

--- app/src/main/java/com/orka/finances/lib/components/Spacers.kt
+++ app/src/main/java/com/orka/finances/lib/components/Spacers.kt
@@ -2,11 +2,9 @@ package com.orka.finances.lib.components
 
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
