7f03d2f77d59f151fb08d3fbe42e84d56637cb68
Karimjon <okracer3350@gmail.com>
2024-12-23 03:28:11 +0500

    Remove fixtures

--- screens/products/src/main/java/com/orka/products/fixtures/UnauthorizerImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.orka.products.fixtures
-
-import com.orka.unauthorizer.Unauthorizer
-
-class UnauthorizerImpl : Unauthorizer {
-    override fun unauthorize() {
-        TODO("Not yet implemented")
-    }
-}
